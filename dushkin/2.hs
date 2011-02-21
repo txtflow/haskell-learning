98265a977049c2a00d11acda29be4529bcb0bc4b
txtflow <txt_flow@mail.ru>
2011-02-21 22:30:15 +0300

    add 2.hs

---
+++ dushkin/2.hs
@@ -0,0 +1,24 @@
+--2.2
+fermat_t :: (Num n, Enum n) => n->n
+fermat_t 0 = 0
+fermat_t n = fermat_t' n 0
+             where fermat_t' n c | n == 0 = c
+                                 | otherwise = fermat_t' (n-1) (c+n)
+                                               
+--1.2
+--fermat_p :: (Num n, Enum n) => n->n
+--fermat_p 0 = 0
+--fermat_p n = fermat_t(n) + fermat_p(n-1)
+
+--1.5
+--getN :: (Num n) => n->[a]->a
+--getN n []     = error ""
+--getN 1 (x:xs) = x
+--getN n (x:xs) = getN (n-1) xs
+
+--1.6
+--atomPosition :: (Num n, Eq a) => a->[a]->n
+--atomPosition a l = atomPosition' a l 1
+--                   where atomPosition' a l n | l == [] = 0 
+--                                             | a == head l = n            
+--                                             | otherwise = atomPosition' a (tail l) (n+1)  
