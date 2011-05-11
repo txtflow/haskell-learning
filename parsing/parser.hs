536ed776473b68a4e267ed9bb2728aaf2318a01b
txtflow <txt_flow@mail.ru>
2011-05-11 23:59:36 +0400

    parsing

--- parsing/parser.hs
+++ parsing/parser.hs
@@ -1,9 +1,12 @@
 module Parser ( ) where
 
+import Control.Monad
+    
+
 data Parser a = Parser (String -> [(a, String)])
 
-(|||) :: forall t. Parser t -> Parser t -> String -> [(t, String)]
-(Parser a) ||| (Parser b) = \s -> a s ++ b s
+-- (|||) :: forall t Parser t -> Parser t -> String -> [(t, String)]
+-- (Parser a) ||| (Parser b) = \s -> a s ++ b s
 
 -- pseq :: Parser a -> (a -> Parser b) -> Parser b
 -- pseq (Parser a) k = \s -> let fres = a s
@@ -32,4 +35,16 @@ instance Monad Parser where
   return a = Parser(\s -> [(a,s)])
   p >>= f  = Parser(\s -> concat [parse (f a) cs' | (a,cs')<-parse p s])
 
-  
+
+
+instance MonadPlus Parser where
+    mzero = Parser (\cs->[])
+    p `mplus` q = Parser (\cs -> parse p cs ++ parse q cs) 
+             
+sat p = do c <- item
+           if p c then return c else mzero
+
+char c = sat (c==)
+
+string ""     = return ""
+string (c:cs) = do{char c; string cs; return (c:cs)}
